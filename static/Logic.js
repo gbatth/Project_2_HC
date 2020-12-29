a8943cb951acd2587b8c28190e2f022ce607756e
gbatth <69115310+gbatth@users.noreply.github.com>
2020-12-29 14:01:37 -0800

    Add files via upload

---
+++ static/Logic.js
@@ -0,0 +1,430 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'tilemap',
+        inverted: true,
+        height: '80%'
+    },
+
+    accessibility: {
+        description: 'A tile map represents the states of the USA by population in 2016. The hexagonal tiles are positioned to geographically echo the map of the USA. A color-coded legend states the population levels as below 1 million (beige), 1 to 5 million (orange), 5 to 20 million (pink) and above 20 million (hot pink). The chart is interactive, and the individual state data points are displayed upon hovering. Three states have a population of above 20 million: California (39.3 million), Texas (27.9 million) and Florida (20.6 million). The northern US region from Massachusetts in the Northwest to Illinois in the Midwest contains the highest concentration of states with a population of 5 to 20 million people. The southern US region from South Carolina in the Southeast to New Mexico in the Southwest contains the highest concentration of states with a population of 1 to 5 million people. 6 states have a population of less than 1 million people; these include Alaska, Delaware, Wyoming, North Dakota, South Dakota and Vermont. The state with the lowest population is Wyoming in the Northwest with 584,153 people.'
+    },
+
+    title: {
+        text: 'U.S. states Median Income (2019-2017)'
+    },
+
+    subtitle: {
+        text: 'Source:US Census Bureau'
+    },
+
+    xAxis: {
+        visible: false
+    },
+
+    yAxis: {
+        visible: false
+    },
+
+    colorAxis: {
+        dataClasses: [{
+            from: 0,
+            to: 50000,
+            color: '#AFEEEE',
+            name: '< 50k'
+        }, {
+            from: 50000,
+            to: 65000,
+            color: '#48d1cc',
+            name: '50k-65k'
+        }, {
+            from: 65000,
+            to: 80000,
+            color: '#20b2aa',
+            name: '65k-80k'
+        }, {
+            from: 80000,
+            color: '#008080',
+            name: '> 80k'
+        }]
+    },
+
+    tooltip: {
+        headerFormat: '',
+        pointFormat: 'The median income of <b> {point.name}</b> is <b>{point.value}</b>'
+    },
+
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.hc-a2}',
+                color: '#000000',
+                style: {
+                    textOutline: false
+                }
+            }
+        }
+    },
+
+    series: [{
+        name: '',
+        data: [{
+            'hc-a2': 'AL',
+            name: 'Alabama',
+            region: 'South',
+            x: 6,
+            y: 7,
+            value: 53363
+        }, {
+            'hc-a2': 'AK',
+            name: 'Alaska',
+            region: 'West',
+            x: 0,
+            y: 0,
+            value: 76570
+        }, {
+            'hc-a2': 'AZ',
+            name: 'Arizona',
+            region: 'West',
+            x: 5,
+            y: 3,
+            value: 65450
+        }, {
+            'hc-a2': 'AR',
+            name: 'Arkansas',
+            region: 'South',
+            x: 5,
+            y: 6,
+            value: 52370
+        }, {
+            'hc-a2': 'CA',
+            name: 'California',
+            region: 'West',
+            x: 5,
+            y: 2,
+            value: 74305
+        }, {
+            'hc-a2': 'CO',
+            name: 'Colorado',
+            region: 'West',
+            x: 4,
+            y: 3,
+            value: 75020
+        }, {
+            'hc-a2': 'CT',
+            name: 'Connecticut',
+            region: 'Northeast',
+            x: 3,
+            y: 11,
+            value: 79639
+        }, {
+            'hc-a2': 'DE',
+            name: 'Delaware',
+            region: 'South',
+            x: 4,
+            y: 9,
+            value: 69378
+        }, {
+            'hc-a2': 'DC',
+            name: 'District of Columbia',
+            region: 'South',
+            x: 4,
+            y: 10,
+            value: 88396
+        }, {
+            'hc-a2': 'FL',
+            name: 'Florida',
+            region: 'South',
+            x: 8,
+            y: 8,
+            value: 56456
+        }, {
+            'hc-a2': 'GA',
+            name: 'Georgia',
+            region: 'South',
+            x: 7,
+            y: 8,
+            value: 57979
+        }, {
+            'hc-a2': 'HI',
+            name: 'Hawaii',
+            region: 'West',
+            x: 8,
+            y: 0,
+            value: 82108
+        }, {
+            'hc-a2': 'ID',
+            name: 'Idaho',
+            region: 'West',
+            x: 3,
+            y: 2,
+            value: 62611
+        }, {
+            'hc-a2': 'IL',
+            name: 'Illinois',
+            region: 'Midwest',
+            x: 3,
+            y: 6,
+            value: 71539
+        }, {
+            'hc-a2': 'IN',
+            name: 'Indiana',
+            region: 'Midwest',
+            x: 3,
+            y: 7,
+            value: 62987
+        }, {
+            'hc-a2': 'IA',
+            name: 'Iowa',
+            region: 'Midwest',
+            x: 3,
+            y: 5,
+            value: 67403
+        }, {
+            'hc-a2': 'KS',
+            name: 'Kansas',
+            region: 'Midwest',
+            x: 5,
+            y: 5,
+            value: 65864
+        }, {
+            'hc-a2': 'KY',
+            name: 'Kentucky',
+            region: 'South',
+            x: 4,
+            y: 6,
+            value: 54337
+        }, {
+            'hc-a2': 'LA',
+            name: 'Louisiana',
+            region: 'South',
+            x: 6,
+            y: 5,
+            value: 49340
+        }, {
+            'hc-a2': 'ME',
+            name: 'Maine',
+            region: 'Northeast',
+            x: 0,
+            y: 11,
+            value: 60626
+        }, {
+            'hc-a2': 'MD',
+            name: 'Maryland',
+            region: 'South',
+            x: 4,
+            y: 8,
+            value: 89658
+        }, {
+            'hc-a2': 'MA',
+            name: 'Massachusetts',
+            region: 'Northeast',
+            x: 2,
+            y: 10,
+            value: 85044
+        }, {
+            'hc-a2': 'MI',
+            name: 'Michigan',
+            region: 'Midwest',
+            x: 2,
+            y: 7,
+            value: 61497
+        }, {
+            'hc-a2': 'MN',
+            name: 'Minnesota',
+            region: 'Midwest',
+            x: 2,
+            y: 4,
+            value: 75841
+        }, {
+            'hc-a2': 'MS',
+            name: 'Mississippi',
+            region: 'South',
+            x: 6,
+            y: 6,
+            value: 44494
+        }, {
+            'hc-a2': 'MO',
+            name: 'Missouri',
+            region: 'Midwest',
+            x: 4,
+            y: 5,
+            value: 60799
+        }, {
+            'hc-a2': 'MT',
+            name: 'Montana',
+            region: 'West',
+            x: 2,
+            y: 2,
+            value: 59597
+        }, {
+            'hc-a2': 'NE',
+            name: 'Nebraska',
+            region: 'Midwest',
+            x: 4,
+            y: 4,
+            value: 68004
+        }, {
+            'hc-a2': 'NV',
+            name: 'Nevada',
+            region: 'West',
+            x: 4,
+            y: 2,
+            value: 64808
+        }, {
+            'hc-a2': 'NH',
+            name: 'New Hampshire',
+            region: 'Northeast',
+            x: 1,
+            y: 11,
+            value: 82866
+        }, {
+            'hc-a2': 'NJ',
+            name: 'New Jersey',
+            region: 'Northeast',
+            x: 3,
+            y: 10,
+            value: 79182
+        }, {
+            'hc-a2': 'NM',
+            name: 'New Mexico',
+            region: 'West',
+            x: 6,
+            y: 3,
+            value: 49943
+        }, {
+            'hc-a2': 'NY',
+            name: 'New York',
+            region: 'Northeast',
+            x: 2,
+            y: 9,
+            value: 68178
+        }, {
+            'hc-a2': 'NC',
+            name: 'North Carolina',
+            region: 'South',
+            x: 5,
+            y: 9,
+            value: 55723
+        }, {
+            'hc-a2': 'ND',
+            name: 'North Dakota',
+            region: 'Midwest',
+            x: 2,
+            y: 3,
+            value: 66830
+        }, {
+            'hc-a2': 'OH',
+            name: 'Ohio',
+            region: 'Midwest',
+            x: 3,
+            y: 8,
+            value: 63569
+        }, {
+            'hc-a2': 'OK',
+            name: 'Oklahoma',
+            region: 'South',
+            x: 6,
+            y: 4,
+            value: 56309
+        }, {
+            'hc-a2': 'OR',
+            name: 'Oregon',
+            region: 'West',
+            x: 4,
+            y: 1,
+            value: 70004
+        }, {
+            'hc-a2': 'PA',
+            name: 'Pennsylvania',
+            region: 'Northeast',
+            x: 3,
+            y: 9,
+            value: 66730
+        }, {
+            'hc-a2': 'RI',
+            name: 'Rhode Island',
+            region: 'Northeast',
+            x: 2,
+            y: 11,
+            value: 67251
+        }, {
+            'hc-a2': 'SC',
+            name: 'South Carolina',
+            region: 'South',
+            x: 6,
+            y: 8,
+            value: 59130
+        }, {
+            'hc-a2': 'SD',
+            name: 'South Dakota',
+            region: 'Midwest',
+            x: 3,
+            y: 4,
+            value: 61384
+        }, {
+            'hc-a2': 'TN',
+            name: 'Tennessee',
+            region: 'South',
+            x: 5,
+            y: 7,
+            value: 57128
+        }, {
+            'hc-a2': 'TX',
+            name: 'Texas',
+            region: 'South',
+            x: 7,
+            y: 4,
+            value: 63661
+        }, {
+            'hc-a2': 'UT',
+            name: 'Utah',
+            region: 'West',
+            x: 5,
+            y: 4,
+            value: 78591
+        }, {
+            'hc-a2': 'VT',
+            name: 'Vermont',
+            region: 'Northeast',
+            x: 1,
+            y: 10,
+            value: 70685
+        }, {
+            'hc-a2': 'VA',
+            name: 'Virginia',
+            region: 'South',
+            x: 5,
+            y: 8,
+            value: 77904
+        }, {
+            'hc-a2': 'WA',
+            name: 'Washington',
+            region: 'West',
+            x: 2,
+            y: 1,
+            value: 79412
+        }, {
+            'hc-a2': 'WV',
+            name: 'West Virginia',
+            region: 'South',
+            x: 4,
+            y: 7,
+            value: 51390
+        }, {
+            'hc-a2': 'WI',
+            name: 'Wisconsin',
+            region: 'Midwest',
+            x: 2,
+            y: 5,
+            value: 65775
+        }, {
+            'hc-a2': 'WY',
+            name: 'Wyoming',
+            region: 'West',
+            x: 3,
+            y: 3,
+            value: 63633
+        }]
+    }]
+});
